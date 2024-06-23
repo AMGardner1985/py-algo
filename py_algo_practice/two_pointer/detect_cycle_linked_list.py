874ee55f99b98d94eac97de9a0ef802ee666cf59
Adam Gardner <AdamGardner33@gmail.com>
2024-06-23 17:21:58 -0400

    add cycle lined list algo

--- py_algo_practice/two_pointer/detect_cycle_linked_list.py
+++ py_algo_practice/two_pointer/detect_cycle_linked_list.py
@@ -5,6 +5,22 @@ class ListNode(object):
         self.next = None
         
 def detectCycle(node: ListNode) -> bool:
-    # TODO
-    # use floyd algo with two pointers
+    
+    
+    slow = node
+    fast = node.next
+    
+    if slow == None or fast == None:
+        return False
+    
+    # continue while both nodes are not null
+    while slow and fast: 
+        # if both nodes are equal then there is a cycle at some point
+        if slow == fast:
+            return False
+        
+        slow = slow.next
+        fast = fast.next.next
+    
+    # will only be reached if one of the noes is null thus there is no cycle
     return True
