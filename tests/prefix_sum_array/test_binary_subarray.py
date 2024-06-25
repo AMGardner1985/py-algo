355871f4daf730a87b8aebd88dc479ec912bd128
Adam Gardner <AdamGardner33@gmail.com>
2024-06-25 12:17:46 -0400

    prefix sum

--- tests/prefix_sum_array/test_binary_subarray.py
+++ tests/prefix_sum_array/test_binary_subarray.py
@@ -2,12 +2,36 @@
 from typing import List
 import pytest
 
+'''
+given you have an array with 1's and 0's and a target/goal integer (e.g. 2)
+
+return the number of subarrays where the sum of those ones and zeros equals my goal
+
+
+basically this is a combo of a sliding window question and a prefix sum question 
+
+'''
+
+
 def test_num_subarray_with_sum():
     input = [1,0,1,0,1]
     goal = 2
     expected_output = 4
     assert num_subarray_with_sum(input, goal) == expected_output
+
+
+
+def subarray_sum(nums : List[int], k: int) -> int:
+    res = 0
+    curSum = 0
+    prefixSums = { 0 : 1 }
     
+    for n in nums:
+        curSum += n
+        diff = curSum - k
+        res += prefixSums.get(diff, 0)
+        prefixSums[curSum] = 1 + prefixSums.get(curSum, 0)
+            
     
 def num_subarray_with_sum(input : List[int], goal : int) -> int:
     total_count = 0
@@ -31,4 +55,4 @@ def num_subarray_with_sum(input : List[int], goal : int) -> int:
     return total_count
         
             
-        
+###
